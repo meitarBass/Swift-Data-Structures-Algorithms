809665e344e2f6cebadf2cfd7e337e1006a8355b
meitarbasson <meitarbass@gmail.com>
2024-10-17 14:56:30 +0300

    Linked List challenges

--- Linked-List.playground/Contents.swift
+++ Linked-List.playground/Contents.swift
@@ -210,11 +210,151 @@ extension LinkedList: Collection {
     }
 }
 
-var list = LinkedList<Int>()
-for i in 0...9 {
-    list.append(i)
+// Challenge 1 - Print linked list in reverse
+
+public func printInReverse<T>(_ node: Node<T>?) {
+    guard let node = node else { return } // nothing to print
+    printInReverse(node.next)
+    print(node.value)
 }
 
-print(list)
+// Challenge 2 - Find the middle node
+
+public func getMiddle<T>(_ list: LinkedList<T>) -> Node<T>? {
+    var slowIterator = list.head
+    var fastIterator = list.head
+    
+    while let nextFastIterator = fastIterator?.next {
+        fastIterator = nextFastIterator.next
+        slowIterator = slowIterator?.next
+    }
+    
+    return slowIterator
+}
+
+
+// Challenge 3 - Reverse a list
+
+extension LinkedList {
+    
+    mutating func reverseTrivial() {
+        var tmpList = LinkedList<Value>()
+        for value in self {
+            tmpList.push(value)
+        }
+        
+        head = tmpList.head
+    }
+    
+    mutating func reverse() {
+        tail = head
+        var prev = head
+        var current = head?.next
+        prev?.next = nil // new end of list
+        
+        while current != nil { // Traversing backwards
+            let next = current?.next
+            current?.next = prev
+            prev = current
+            current = next
+        }
+        
+        head = prev
+    }
+}
+
+// Challenge 4 - Merge two ordered lists
+
+// Need to use comparable in order to compare T and Value
+func mergeLists<T: Comparable>(_ leftList: LinkedList<T>, _ rightList: LinkedList<T>) -> LinkedList<T>? {
+    guard !leftList.isEmpty else { return rightList }
+    guard !rightList.isEmpty else { return leftList }
+    
+    var newHead: Node<T>?
+    var tail: Node<T>?
+    var currentLeft = leftList.head
+    var currentRight = rightList.head
+    
+    if let leftNode = currentLeft, let rightNode = currentRight {
+        if leftNode.value < rightNode.value {
+            newHead = leftNode
+            currentLeft = leftNode.next
+        } else {
+            newHead = rightNode
+            currentRight = rightNode.next
+        }
+        tail = newHead
+    }
+    
+    while let leftNode = currentLeft, let rightNode = currentRight {
+        if leftNode.value < rightNode.value {
+            tail?.next = leftNode
+            currentLeft = leftNode.next
+        } else {
+            tail?.next = rightNode
+            currentRight = rightNode.next
+        }
+        tail = tail?.next
+    }
+    
+    if let leftNodes = currentLeft {
+        tail?.next = leftNodes
+    }
+    
+    if let rightNodes = currentRight {
+        tail?.next = rightNodes
+    }
+    
+    var list = LinkedList<T>()
+    list.head = newHead
+    list.tail = {
+        while let next = tail?.next {
+            tail = next
+        }
+        return tail
+    }()
+    return list
+}
+
+// Challenge 5 - Remove all occurrences
+
+// Need to use comparable in order to compare T and Value
+extension LinkedList where Value: Equatable {
+    public mutating func removeAllOccurrences(of value: Value) {
+        // remove head case
+        while let head = head, head.value == value {
+            self.head = head.next
+        }
+        
+        var prev = head
+        var current = head?.next
+        
+        while let currentNode = current {
+            guard currentNode.value != value else {
+                prev?.next = currentNode.next
+                current = prev?.next
+                continue
+            }
+            
+            prev = current
+            current = current?.next
+        }
+        
+        tail = prev
+    }
+}
+
+var list = LinkedList<Int>()
+list.append(1)
+list.append(2)
+list.append(2)
+list.append(3)
+list.append(2)
+
+print("Before removal: \(list)")
+list.removeAllOccurrences(of: 2)
+print("After removal: \(list)")
+
+
 
 
