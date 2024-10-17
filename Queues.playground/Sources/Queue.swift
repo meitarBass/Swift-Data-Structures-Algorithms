dfb71e3f1702b55c278a7149ac0694fcfd6fda4a
meitarbasson <meitarbass@gmail.com>
2024-10-17 17:37:05 +0300

    Added all queues version

---
+++ Queues.playground/Sources/Queue.swift
@@ -0,0 +1,8 @@
+public protocol Queue {
+    // Placeholder for type that will conform to protocol
+    associatedtype Element
+    mutating func enqueue(_ element: Element) -> Bool
+    mutating func dequeue() -> Element?
+    var isEmpty: Bool { get }
+    var peek: Element? { get }
+}
